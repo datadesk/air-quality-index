eb4f8bff5a981e4bd1f2f385373ac8e102c8d608
palewire <b@palewi.re>
2021-12-06 05:34:02 -0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='air-quality-index',
-    version='0.1.0',
+    version='0.1.1',
     description="Download air quality index data from AirNow",
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
